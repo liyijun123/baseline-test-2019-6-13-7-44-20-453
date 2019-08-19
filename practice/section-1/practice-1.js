13f9d78310193b83751ec7a960987e1937798199
liyijun123 <52227521+liyijun123@users.noreply.github.com>
2019-08-19 18:41:08 +0800

    Update practice-1.js

--- practice/section-1/practice-1.js
+++ practice/section-1/practice-1.js
@@ -1,5 +1,14 @@
+    
 'use strict';
 
 function collectSameElements(collectionA, collectionB) {
-  return '实现练习要求，并改写该行代码。';
+  let newVar = [];
+        for (let i = 0; i < collectionA.length; i++) {
+            for (let j = 0; j < collectionB.length; j++) {
+                if(collectionB[j] === collectionA[i]){
+                    newVar.push(collectionB[j]);
+                }
+        }
+     }
+  return newVar;
 }
